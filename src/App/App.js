50c5355f36949d745203ea4240924bd6c3fb1ae5
Tyler Sharp <johntylersharp@gmail.com>
2022-11-04 00:55:31 -0400

    refactored into leaflet, trying to get location recognized

--- src/App/App.js
+++ src/App/App.js
@@ -2,27 +2,47 @@ import React, { useRef, useEffect, useState } from "react";
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import mapboxgl from "!mapbox-gl";
 import "./App.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import locationData from "../assets/data/locationManifest.geojson";
 
-mapboxgl.accessToken =
-  "pk.eyJ1IjoicmNyZWF0aXZldHlsZXIiLCJhIjoiY2w5aXUxdzh5M21tbDN1bXZ0aGdwMW82biJ9.SA-skD52q21g6p_5_OMAaw";
+const envVariables = process.env;
+
+mapboxgl.accessToken = envVariables.REACT_APP_MAPBOX_TOKEN;
 
 function App() {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
+  const [lng, setLng] = useState(33.6615);
+  const [lat, setLat] = useState(-78.9379);
   const [zoom, setZoom] = useState(9);
 
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [lng, lat],
-      zoom: zoom,
-    });
-  });
+  // useEffect(() => {
+  //   if (map.current) return; // initialize map only once
+  //   map.current = new mapboxgl.Map({
+  //     container: mapContainer.current,
+  //     style: "mapbox://styles/mapbox/streets-v11",
+  //     center: [lng, lat],
+  //     zoom: zoom,
+  //   });
+  // });
 
+  // useEffect(() => {
+  //   map.current.on("load", () => {
+  //     mapboxgl.addSource("locations", {
+  //       type: "geojson",
+  //       data: "../assets/data/locationManifest.geojson",
+  //     });
+  //   });
+  // });
+
+  const handleMove = () => {
+    setLng(map.current.getCenter().lng.toFixed(4));
+    setLat(map.current.getCenter().lat.toFixed(4));
+  };
+
+  const handleZoom = () => {
+    setZoom(map.current.getZoom().toFixed(2));
+  };
   useEffect(() => {
     if (!map.current) return; // wait for map to initialize
     map.current.on("move", () => {
@@ -33,12 +53,25 @@ function App() {
   });
 
   return (
-    <div>
-      <div ref={mapContainer} className="map-container" />
+    <MapContainer
+      center={[lng, lat]}
+      zoom={zoom}
+      scrollWheelZoom={true}
+      onZoom={handleZoom}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[lng, lat]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
       <div className="sidebar">
         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
       </div>
-    </div>
+    </MapContainer>
   );
 }
 
