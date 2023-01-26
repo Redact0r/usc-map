6746766b8985eadc01b0bdfecb5b4338cf29a7f8
Tyler Sharp <johntylersharp@gmail.com>
2023-01-26 16:30:09 -0500

    Some datapoints were erroneous, generating on different places on the world map, causing lag. cleaned data and further revised doc for USC.

--- src/Components/MainMap/MainMap.js
+++ src/Components/MainMap/MainMap.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { MapContainer, GeoJSON } from "react-leaflet";
+import {
+  MapContainer,
+  GeoJSON,
+  TileLayer,
+  Marker,
+  MarkerProps,
+  Popup,
+} from "react-leaflet";
 import pointsOfInterests from "../../assets/data/locationManifest.json";
 import "leaflet/dist/leaflet.css";
 import "./MainMap.css";
@@ -19,12 +26,29 @@ class MainMap extends React.Component {
     });
   }
 
+  onEachPOI = (poi, layer) => {
+    const poiName = poi.properties.Name;
+
+    layer.bindPopup(poiName);
+  };
+
   render() {
     return (
       <div>
         <h1>Map</h1>
-        <MapContainer style={{ height: "80vh" }} zoom={2} center={[20, 100]}>
-          <GeoJSON data={pointsOfInterests.features} />
+        <MapContainer
+          style={{ height: "80vh" }}
+          zoom={2}
+          center={[-78.9379, 33.66155]}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <GeoJSON
+            data={pointsOfInterests.features}
+            onEachFeature={this.onEachPOI}
+          />
         </MapContainer>
       </div>
     );
