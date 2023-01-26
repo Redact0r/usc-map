5fd0fb85403e16a0bbe0f434cbc307c0fe1dd2dd
Tyler Sharp <johntylersharp@gmail.com>
2023-01-26 17:30:27 -0500

    Refactored from class components into functional components. Had a mix of the two.

--- src/Components/MainMap/MainMap.js
+++ src/Components/MainMap/MainMap.js
@@ -7,52 +7,29 @@ import {
   MarkerProps,
   Popup,
 } from "react-leaflet";
-import pointsOfInterests from "../../assets/data/locationManifest.json";
+import pointsOfInterest from "../../assets/data/locationManifest.json";
 import "leaflet/dist/leaflet.css";
 import "./MainMap.css";
 
-class MainMap extends React.Component {
-  state = {};
+function MainMap() {
+  return (
+    <MapContainer
+      style={{ height: "100vh", width: "100vw" }}
+      zoom={7}
+      center={[33.66155, -78.9379]}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
 
-  componentDidMount() {
-    const listOfFeatures = pointsOfInterests.features;
-
-    listOfFeatures.forEach((feature) => {
-      const coordinates = feature.geometry.coordinates;
-
-      if (!Array.isArray(coordinates)) {
-        console.log(`${feature.properties.Name} coordinates are not an array`);
-      }
-    });
-  }
-
-  onEachPOI = (poi, layer) => {
-    const poiName = poi.properties.Name;
-
-    layer.bindPopup(poiName);
-  };
-
-  render() {
-    return (
-      <div>
-        <h1>Map</h1>
-        <MapContainer
-          style={{ height: "80vh" }}
-          zoom={2}
-          center={[-78.9379, 33.66155]}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <GeoJSON
-            data={pointsOfInterests.features}
-            onEachFeature={this.onEachPOI}
-          />
-        </MapContainer>
-      </div>
-    );
-  }
+      {pointsOfInterest.features.map((poi, idx) => {
+        const lat = poi.geometry.coordinates[1];
+        const lon = poi.geometry.coordinates[0];
+        return <Marker key={idx} position={[lat, lon]}></Marker>;
+      })}
+    </MapContainer>
+  );
 }
 
 export default MainMap;
