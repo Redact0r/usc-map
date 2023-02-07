d6e9c4924dec796338624c311c9354730611fddb
Tyler Sharp <johntylersharp@gmail.com>
2023-02-06 23:31:24 -0500

    Location marker added for current location

--- src/Components/CurrentLocation/CurrentLocation.js
+++ src/Components/CurrentLocation/CurrentLocation.js
@@ -1,23 +1,26 @@
-import { useState } from "react";
-import { useMapEvents } from "react-leaflet";
+import { useState, useEffect } from "react";
+import { useMap, useMapEvents, CircleMarker, Popup } from "react-leaflet";
 
 const CurrentLocation = () => {
   const [position, setPosition] = useState(null);
 
-  const map = useMapEvents({
-    click() {
-      map.locate();
-    },
-    locationfound(e) {
-      console.log(e);
-      const lat = e.latlng.lat;
-      const lng = e.latlng.lng;
-      setPosition([lat, lng]);
-      map.flyTo(e.latlng, map.getZoom());
-    },
-  });
+  const map = useMap();
 
-  return position;
+  useEffect(() => {
+    map.locate().on("locationfound", (e) => {
+      const pos = [e.latlng.lat, e.latlng.lng];
+      setPosition(pos);
+      map.flyTo(pos, map.getZoom());
+    });
+  }, [map]);
+
+  return position === null ? null : (
+    <CircleMarker center={position}>
+      <Popup>
+        <p>You are here</p>
+      </Popup>
+    </CircleMarker>
+  );
 };
 
 export default CurrentLocation;
